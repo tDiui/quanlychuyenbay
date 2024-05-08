a942b6b101cb0a372f91ec243d9a3679e77d6854
hadangkhoa <hadangkhoasss@gmail.com>
2024-05-08 20:52:12 +0700

    Them Folder BaoCaoCuoiKy
    bat dau chinh sua project

---
+++ BaoCaoCuoiKy/DatChuyenBay.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaoCaoCuoiKy
+{
+    public partial class DatChuyenBay : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-JTO2V7H;Initial Catalog=qlSanBay;Integrated Security=True");
+        public DatChuyenBay()
+        {
+            InitializeComponent();
+        }
+
+        private void DatChuyenBay_Load(object sender, EventArgs e)
+        {
+            string query = "select * from chuyenbay";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            List<string> ls = new List<string>();
+            conn.Open();
+            SqlDataAdapter data = new SqlDataAdapter(cmd);
+            DataTable db = new DataTable();
+            data.Fill(db);
+            
+            
+        }
+    }
+}
---
+++ BaoCaoCuoiKy/TrangChu.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace BaoCaoCuoiKy
+{
+    partial class TrangChu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrangChu));
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            label2 = new Label();
+            label1 = new Label();
+            MatKhauBox = new TextBox();
+            TaiKhoanBox = new TextBox();
+            ThoatBtn = new Button();
+            DatChuyenBayBtn = new Button();
+            DangNhapBtn = new Button();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(32, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(125, 104);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.AppWorkspace;
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(MatKhauBox);
+            panel1.Controls.Add(TaiKhoanBox);
+            panel1.Controls.Add(ThoatBtn);
+            panel1.Controls.Add(DatChuyenBayBtn);
+            panel1.Controls.Add(DangNhapBtn);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(194, 451);
+            panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label2.Location = new Point(13, 197);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Mật khẩu:";
+            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label1.Location = new Point(13, 136);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Tài khoản:";
+            // 
+            // MatKhauBox
+            // 
+            MatKhauBox.Location = new Point(13, 224);
+            MatKhauBox.Name = "MatKhauBox";
+            MatKhauBox.Size = new Size(167, 27);
+            MatKhauBox.TabIndex = 3;
+            MatKhauBox.TextChanged += textBox2_TextChanged;
+            // 
+            // TaiKhoanBox
+            // 
+            TaiKhoanBox.Location = new Point(13, 165);
+            TaiKhoanBox.Name = "TaiKhoanBox";
+            TaiKhoanBox.Size = new Size(168, 27);
+            TaiKhoanBox.TabIndex = 2;
+            // 
+            // ThoatBtn
+            // 
+            ThoatBtn.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            ThoatBtn.Location = new Point(3, 396);
+            ThoatBtn.Name = "ThoatBtn";
+            ThoatBtn.Size = new Size(185, 41);
+            ThoatBtn.TabIndex = 1;
+            ThoatBtn.Text = "Thoát";
+            ThoatBtn.UseVisualStyleBackColor = true;
+            ThoatBtn.Click += ThoatBtn_Click;
+            // 
+            // DatChuyenBayBtn
+            // 
+            DatChuyenBayBtn.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            DatChuyenBayBtn.Location = new Point(3, 349);
+            DatChuyenBayBtn.Name = "DatChuyenBayBtn";
+            DatChuyenBayBtn.Size = new Size(185, 41);
+            DatChuyenBayBtn.TabIndex = 1;
+            DatChuyenBayBtn.Text = "Đặt chuyến bay";
+            DatChuyenBayBtn.UseVisualStyleBackColor = true;
+            DatChuyenBayBtn.Click += DatChuyenBayBtn_Click;
+            // 
+            // DangNhapBtn
+            // 
+            DangNhapBtn.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            DangNhapBtn.Location = new Point(43, 264);
+            DangNhapBtn.Name = "DangNhapBtn";
+            DangNhapBtn.Size = new Size(100, 30);
+            DangNhapBtn.TabIndex = 1;
+            DangNhapBtn.Text = "Đăng nhập";
+            DangNhapBtn.UseCompatibleTextRendering = true;
+            DangNhapBtn.UseVisualStyleBackColor = true;
+            DangNhapBtn.Click += DangNhapBtn_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(200, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(290, 209);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(496, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(298, 209);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(200, 215);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(290, 236);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 2;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(496, 215);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(298, 236);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 2;
+            pictureBox5.TabStop = false;
+            // 
+            // TrangChu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(panel1);
+            MaximizeBox = false;
+            Name = "TrangChu";
+            Text = "Đăng nhập hệ thống";
+            Load += RegisterForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private Button ThoatBtn;
+        private Button DatChuyenBayBtn;
+        private Button DangNhapBtn;
+        private Label label2;
+        private Label label1;
+        private TextBox MatKhauBox;
+        private TextBox TaiKhoanBox;
+    }
+}
