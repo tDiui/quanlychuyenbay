7fdb4fbb9730c165c966a2d9e7ad328059934d10
hadangkhoa <hadangkhoasss@gmail.com>
2024-05-19 16:56:31 +0700

    update

---
+++ BaoCaoCuoiKy/DataProvider/TaiKhoan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaoCaoCuoiKy.DataProvider
+{
+    internal class TaiKhoan
+    {
+        private string tenTK;
+        private string MK;
+
+        public TaiKhoan()
+        {
+
+        }
+        public TaiKhoan(string tenTK, string MK)
+        {
+            this.tenTK = tenTK;
+            this.MK = MK;
+        }
+        public string getTenTK() { return tenTK; }
+        public string getMK() { return MK; }
+        public void setTenTK(string tenTK) { this.tenTK = tenTK; }
+        public void setMK(string MK) { this.MK = MK; }
+
+    }
+}
+
---
+++ BaoCaoCuoiKy/TaiTrang.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaoCaoCuoiKy
+{
+    public partial class TaiTrang : Form
+    {
+        private int currentImageIndex = 0;
+        static string path = Application.StartupPath + "../../../"; //không thay đổi cái path này nhaaa
+        private string[] imagePaths = { @$"{path}\Resources\anh-may-bay-dang-bay.jpg", @$"{path}\Resources\hinh-anh-bay-tren-troi-la-may-bay_111631532.jpg", @$"{path}\Resources\anh-may-bay-dep_041752800.jpg", @$"{path}\Resources\hinh-anh-may-bay-phan-luc.jpg" };
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-JTO2V7H;Initial Catalog=qlSanBay;Integrated Security=True");
+        public TaiTrang()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage; // Để ảnh hiển thị đầy đủ trong PictureBox
+            timer1.Interval = 1000; // 1 giây
+            timer1.Tick += Timer_Tick;
+            timer1.Start();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = imagePaths[currentImageIndex];
+        }
+
+
+
+        private void RunProgressBar(object sender, KeyPressEventArgs e)
+        {
+            pB1.Value = 30;
+            pB1.Maximum = 100;
+            pB1.Minimum = 0;
+            int num = 10;
+            for (int i = 0; i < 7; i++)
+            {
+                pB1.Value += num;
+                Thread.Sleep(200);
+            }
+
+            TrangChu rf = new TrangChu();
+            rf.Show();
+            this.Hide();
+            rf.Closed += (sender, e) => this.Close();
+
+        }
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            // Chuyển đổi ảnh
+            currentImageIndex = (currentImageIndex + 1) % imagePaths.Length;
+            pictureBox1.ImageLocation = imagePaths[currentImageIndex];
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
